95acbb51a6d559264be06da913a003f6bfc715f0
Murvyn <marvin.asamoah.123@gmail.com>
2025-04-15 00:16:19 +0000

    catching exceptions

--- ExceptionsDemo.java
+++ ExceptionsDemo.java
@@ -1,6 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show() {
-        sayHello(null);
+        try {
+            var reader = new FileReader("file.txt");
+            System.out.println("File opened successfully.");
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found: " + ex.getMessage());
+        }
     }
 
     public static void sayHello(String name) {
